d875b7613ddff233bbe704490b4e5cfd8ae58118
sabenih726 <sabenih726@gmail.com>
2025-05-07 12:19:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
-    historyApiFallback: true,  // Menambahkan historyApiFallback
+    historyApiFallback: true // <- tambahkan ini jika develop di lokal
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+})
